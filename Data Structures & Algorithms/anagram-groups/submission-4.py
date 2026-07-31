16add71780a48cf0bf642530cb1e82236dc7f2c1
Saadullah-2004 <126239263+Saadullah-2004@users.noreply.github.com>
2026-07-31 19:04:21 -0400

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Now instead of having deadicated lists we have track the total character by using ord
+        from collections import defaultdict
+        ans = defaultdict(list)
+
+        for strings in strs:
+            anagram = [0] * 26
+            for char in strings:
+                anagram[ord(char) - 97] +=1
+            ans[tuple(anagram)].append(strings)
+
+        return list(ans.values())
